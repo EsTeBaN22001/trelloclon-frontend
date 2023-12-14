b3c557bb350be5934ee9f061dc3526e46fddba96
Esteban Redon <esteban1.redon2@gmail.com>
2023-12-14 12:41:50 -0300

    Se agregó la funcionalidad de crear una nueva board

---
+++ src/app/modules/layout/components/board-form/board-form.component.ts
@@ -0,0 +1,50 @@
+import { Component, OnInit } from '@angular/core'
+import { FormBuilder, FormControl, Validators } from '@angular/forms'
+import { Router } from '@angular/router'
+import { faCheck } from '@fortawesome/free-solid-svg-icons'
+import { Colors } from '@models/colors.model'
+import { BoardsService } from '@services/boards.service'
+
+@Component({
+  selector: 'app-board-form',
+  templateUrl: './board-form.component.html'
+})
+export class BoardFormComponent implements OnInit {
+  // FontAwesome Icons
+  faCheck = faCheck
+
+  // BackgroundColor variable
+  backgroundColor: Colors = 'sky'
+
+  form = this.formBuilder.group({
+    title: ['', [Validators.required]],
+    backgroundColor: new FormControl<Colors>('sky', {
+      nonNullable: true,
+      validators: [Validators.required]
+    })
+  })
+
+  constructor(
+    private formBuilder: FormBuilder,
+    private boardsService: BoardsService,
+    private router: Router
+  ) {}
+
+  ngOnInit(): void {
+    this.form.get('backgroundColor')?.valueChanges.subscribe(color => {
+      this.backgroundColor = color as Colors
+      console.log(this.backgroundColor)
+    })
+  }
+
+  onSave() {
+    if (this.form.valid) {
+      const { title, backgroundColor } = this.form.getRawValue()
+      this.boardsService.createBoard(title ?? '', backgroundColor).subscribe(board => {
+        this.router.navigate([`/app/boards/${board.id}`])
+      })
+    } else {
+      this.form.markAllAsTouched()
+    }
+  }
+}
--- src/app/modules/layout/components/navbar/navbar.component.ts
+++ src/app/modules/layout/components/navbar/navbar.component.ts
@@ -19,6 +19,7 @@ export class NavbarComponent implements OnInit {
 
   isOpenOverlayAvatar = false
   isOpenOverlayBoards = false
+  isOpenOverlayCreateBoard = false
 
   constructor(
     private authService: AuthService,
