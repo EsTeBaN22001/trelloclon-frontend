7ecb97377feebebe881fbbd42e030c3eb0d5584f
Esteban <esteban1.redon2@gmail.com>
2025-02-27 20:49:01 -0300

    Se agregó el listado de boards en el menu flotante del navbar

--- src/app/modules/layout/components/navbar/navbar.component.ts
+++ src/app/modules/layout/components/navbar/navbar.component.ts
@@ -1,11 +1,12 @@
 import { Component, OnInit } from '@angular/core'
-import { Router } from '@angular/router'
 import { faBell, faInfoCircle, faClose, faAngleDown } from '@fortawesome/free-solid-svg-icons'
+import { Board } from '@models/board.model'
 import { Colors } from '@models/colors.model'
 import { User } from '@models/user.model'
 import { AuthService } from '@services/auth.service'
 import { BoardsService } from '@services/boards.service'
 import { TokenService } from '@services/token.service'
+import { UsersService } from '@services/users.service'
 
 @Component({
   selector: 'app-navbar',
@@ -13,6 +14,8 @@ import { TokenService } from '@services/token.service'
 })
 export class NavbarComponent implements OnInit {
   user: User | null = null
+
+  boards: Board[] = []
 
   bgColor: Colors = 'sky'
 
@@ -27,8 +30,8 @@ export class NavbarComponent implements OnInit {
 
   constructor(
     private authService: AuthService,
-    private tokenService: TokenService,
-    private boardService: BoardsService
+    private boardService: BoardsService,
+    private usersService: UsersService
   ) {}
 
   ngOnInit(): void {
@@ -36,14 +39,15 @@ export class NavbarComponent implements OnInit {
     this.boardService.backgroundColor$.subscribe(color => {
       this.bgColor = color
     })
+
+    this.usersService.getBoards().subscribe(boards => {
+      this.boards = boards
+    })
   }
 
   logout() {
     this.authService.logout()
     window.location.reload()
-  }
-
-  isValidToken() {
   }
 
   // Recibiendo el output del app-board-form (component)
